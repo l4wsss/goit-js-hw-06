a5b3653e49cebdf4c6541462e2a2e7ad5fbbe998
l4wsss <122654266+l4wsss@users.noreply.github.com>
2023-08-27 13:14:37 +0300

    ver 1.1

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,8 @@ const categoriesList = document.querySelectorAll("ul#categories > li.item");
 console.log("Number of categories:", categoriesList.length);
 
 categoriesList.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const categoryEl = category.querySelectorAll("li");
+  const categoryName = category.firstElementChild.textContent;
+  const categoryEl = category.lastElementChild.querySelectorAll("li");
 
   console.log(`\nCategory: ${categoryName}`);
   console.log(`Elements: ${categoryEl.length}`);
